0178947896b0da761820e5ad5ffa8708e48cb23a
Emerson Gomes Rebolsas <emerson.rebolsas@sisconsultoria.com.br>
2023-03-25 00:44:35 -0300

    create fetch functions

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,29 +1,27 @@
 import axios, { AxiosRequestConfig } from 'axios'
+import { TYPES_ACTIONS } from 'contexts/PhoneBook'
 
 const baseUrl = 'http://teste-frontend.saperx.com.br/api/schedule'
 
-interface ICreateContact {
-  name: string
-  numbers: string[]
-  email: string
-  cpf: string
-  date_born: string
+interface IResolvedRequest {
+  url?: string
+  method?: string
+  data?: object
 }
 
 export const resolvedRequest = async ({
   url = baseUrl,
-  method = 'POST',
-  body = {}
-}) => {
-  console.log({ body })
-
+  method = 'GET',
+  data = {}
+}: IResolvedRequest = {}): Promise<AxiosRequestConfig> => {
+  console.log({ data })
   const axiosConfig: AxiosRequestConfig<any> = {
     method,
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
     },
-    data: JSON.stringify(body)
+    data: JSON.stringify(data)
   }
   try {
     const response = await axios(url, axiosConfig)
@@ -33,19 +31,35 @@ export const resolvedRequest = async ({
   }
 }
 
-export const fetchContacts = async () => {
-  return resolvedRequest({
-    method: 'GET'
+export const fetchContacts = async (): Promise<AxiosRequestConfig> => {
+  return resolvedRequest()
+}
+
+export const createContact = async (data: ICreateContact) => {
+  await resolvedRequest({
+    data,
+    method: 'POST'
+  })
+}
+export const editContact = async (data: IResponseCreateContact) => {
+  await resolvedRequest({
+    method: 'PUT',
+    url: `${baseUrl}/${data.id}`,
+    data
   })
 }
 
-export const createContact = async (body: ICreateContact) =>
-  resolvedRequest({
-    body
-  })
-
-export const deleteContact = async () =>
-  resolvedRequest({
+export const deleteContact = async (id: number) => {
+  await resolvedRequest({
     method: 'DELETE',
-    url: `${baseUrl}/83`
+    url: `${baseUrl}/${id}`
   })
+}
+
+export const reFetchContacts = (dispatch: React.Dispatch<any>) => {
+  setTimeout(() => {
+    dispatch({
+      type: TYPES_ACTIONS.REFRESH_CONTACTS
+    })
+  }, 2000)
+}

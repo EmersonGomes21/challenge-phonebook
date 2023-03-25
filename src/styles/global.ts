6233a9a0f1be9edc57d569e0db89d404df394d63
Emerson Gomes Rebolsas <emerson.rebolsas@sisconsultoria.com.br>
2023-03-25 00:41:56 -0300

    add types global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,7 +15,9 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
-
+.text-larger {
+    font-size: 1.56rem;
+}
 `
 
 export default GlobalStyles

2bec3978204cb8d093afc7daf8b8fa7e7ba5f06a
Emerson Gomes Rebolsas <emerson.rebolsas@sisconsultoria.com.br>
2023-03-25 00:47:18 -0300

    Create context with reduce state

---
+++ src/contexts/PhoneBook/index.tsx
@@ -0,0 +1,55 @@
+import { isEmpty } from 'lodash'
+import React, { createContext, useContext, useReducer } from 'react'
+
+interface IContact {
+  name: string
+  numbers: string[]
+  email: string
+  cpf: string
+  date_born: string
+  id: string
+}
+interface IUsePhoneBook {
+  state: {
+    contacts: IContact[]
+    refresh: boolean
+  }
+  dispatch: React.Dispatch<any>
+}
+export const ContextPhoneBook = createContext({} as IUsePhoneBook)
+
+const initialState = {
+  contacts: [],
+  refresh: false
+}
+export const TYPES_ACTIONS = {
+  FETCH_CONTACTS: 'FETCH_CONTACTS',
+  REFRESH_CONTACTS: 'REFRESH_CONTACTS'
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case TYPES_ACTIONS.FETCH_CONTACTS:
+      return { ...state, contacts: action.payload.data }
+    case TYPES_ACTIONS.REFRESH_CONTACTS:
+      return { ...state, refresh: !state.refresh }
+    default:
+      return state
+  }
+}
+
+export const PhoneBookProvider = ({ children }: any) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <ContextPhoneBook.Provider value={{ state, dispatch }}>
+      {children}
+    </ContextPhoneBook.Provider>
+  )
+}
+
+export const usePhoneBook = (): IUsePhoneBook => {
+  const context = useContext(ContextPhoneBook)
+  if (isEmpty(context)) throw new Error('Context not found')
+  return { ...context }
+}
